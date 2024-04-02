1b186d596e457b192c43a54f633c9efe8e2fb83d
Rayson Zheng <121279523+raysonzheng10@users.noreply.github.com>
2024-04-02 13:52:18 -0400

    connected to YT API, able to grab the top comments from any yt video

--- youtubeAPI.py
+++ youtubeAPI.py
@@ -1,16 +1,19 @@
 import requests
 import json
 
+# Youtube APIKey was created from the YT app
 YOUTUBE_APIKEY = 'AIzaSyBEzevk5Am8U4xhlgdTmMmuFRB7FtbIDQw'
+
+# we create the request url using the apikey and we can get the video_id the youtube video url
 baseURL = 'https://www.googleapis.com/youtube/v3'
+video_id = 'cqGjhVJWtEg' # 
+url = f'{baseURL}/commentThreads?part=snippet&videoId={video_id}&key={YOUTUBE_APIKEY}&order=relevance'
 
-video_id = 'cqGjhVJWtEg'
-url = f'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={video_id}&key={YOUTUBE_APIKEY}'
-
+# make the request and store the data
 response = requests.get(url)
 data = json.loads(response.text)
 
+# parse through the json and grab the actual comment itself
 items = data["items"]
-print(items)
-for comment in items:
-    print(comment['snippet']['topLevelComment']['snippet']['textDisplay'])
+for item in items:
+    print(item['snippet']['topLevelComment']['snippet']['textDisplay']) 
