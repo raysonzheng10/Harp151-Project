40865541d0db4ff1c8cd6e9e0beedcc5c61bf5a7
Jomin Zhang <jominzhang77@gmail.com>
2024-04-03 18:17:18 -0400

    grabs google reviews

--- googleReviews.py
+++ googleReviews.py
@@ -21,8 +21,57 @@ class GoogleReviews:
         input_element = self.driver.find_element(By.CLASS_NAME, "gLFyf")
         input_element.clear()
         input_element.send_keys(f"{movie} reviews" + Keys.ENTER)
+        
+        #Finds the review box and clicks into it 
+        try:
+            WebDriverWait(self.driver,5).until(
+                EC.presence_of_element_located((By.CLASS_NAME,"e8eHnd"))
+            )
+            self.driver.find_element(By.CLASS_NAME,"e8eHnd").click()
+        except:
+            print("movie reviews not found")
 
-        time.sleep(20)
+        #Finds the more button and clicks it
+        #For long movie reviews google won't initially show the whole review
+        #Probably will want to make it so this clicks all the more buttons to load them
+        #So they can be grabbed later?
+        try:
+            WebDriverWait(self.driver,5).until(
+                EC.presence_of_element_located((By.CLASS_NAME,"tEJZ0b"))
+            )
+            self.driver.find_element(By.CLASS_NAME,"tEJZ0b").click()
+        except:
+            print("")
+        
+        #Finds the movie reviews
+        #All review reviews are class name "T7nuU"
+        WebDriverWait(self.driver,10).until(
+            EC.presence_of_element_located((By.CLASS_NAME,"T7nuU"))
+        )
+        """
+        Array of movie reviews
+        Every even interval is the shortened version of the review
+        The odd intervals are the full versions of the review
+        However if the more button for that review isn't pressed, the review won't be loaded
+        If the more button is pressed, the short review gets unloaded? and the even intervals will end up empty
+        I'm not sure how to deal with the edge cases where a short review shows up
+        It won't have the more option and it will throw off the indexing
+        Can be put into a for loop to grab x amount of reviews
+        """
+        print(self.driver.find_elements(By.CLASS_NAME,"T7nuU")[1].text)
+        time.sleep(5)
+    
+    """
+    Function to get movie reviews filtered by rating (1-5 stars)
+    Currently not sure how to actually click into each specific rating
+    def get_movie_reviews_ratings(rating):
+        #This will find the filter box and click it
+        WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "zbTRdb"))
+        )
+        filter_box = driver.find_element(By.CLASS_NAME,"zbTRdb")
+        filter_box.click()
+    """
 
 
 googleReviews = GoogleReviews()
