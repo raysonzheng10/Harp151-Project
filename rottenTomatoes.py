898cfdadc91d4d002cadfd48e4d6059904230ca9
Rayson Zheng <121279523+raysonzheng10@users.noreply.github.com>
2024-04-08 11:28:47 -0400

    got basic scraping for rotten tomatoes working

--- rottenTomatoes.py
+++ rottenTomatoes.py
@@ -3,18 +3,28 @@ from bs4 import BeautifulSoup
 
 class RottenTomatoes:
     def __init__(self):
-        self.baseURL = 'https://www.rottentomatoes.com/m'
+        self.baseURL = 'https://www.rottentomatoes.com'
 
-    def get_topCritic_reviews(self):
-        pass
+    # function to format the name of a movie_title to get put into the url for bs4
+    def format_movie_title(self, movie_title:str ) -> str:
+        return movie_title.replace(" ", "_").replace("-", "_")
+    
+    # returns a list of top critic reviews for a given movie title
+    # TODO: be able to click the 'more reviews' button to get more of them
+    def get_topCritic_reviews(self, movie_title: str ) -> list[str]:
+        reformatted_title = self.format_movie_title(movie_title)
+        url = f'{self.baseURL}/m/{reformatted_title}/reviews?intcmp=rt-scorecard_tomatometer-reviews'
 
+        source = requests.get(url).text
+        soup = BeautifulSoup(source, 'html.parser')
+        
+        reviews = []
 
-url = "https://www.rottentomatoes.com/m/the_dark_knight/reviews?intcmp=rt-scorecard_tomatometer-reviews"
-user_agent = {'User-agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=user_agent).text
-soup = BeautifulSoup(response, 'html.parser')
-print(soup.prettify())
+        for element in soup.find_all('div', class_='review-row'):
+            review_text = element.find('p', class_='review-text').text
+            reviews.append(review_text)
 
-for element in soup.find_all('div', class_='review-row'):
-    print(element)
-print("Completed")
+        return reviews
+
+tomatoes = RottenTomatoes()
+tomatoes.get_topCritic_reviews('spiderman')
